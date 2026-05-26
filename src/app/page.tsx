eaebc4cf0d9a27002477a26b2df2b67d7d73805d
sorincenusa <sorincenusa@gmail.com>
2026-05-26 12:30:22 +0300

    Update paragraph margins in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -161,8 +161,11 @@ Datele tale sunt confidențiale și nu vor fi vândute, distribuite sau transmis
 Luăm măsuri rezonabile pentru protejarea și securizarea informațiilor transmise prin intermediul acestui site. Datele vor fi șterse în cursul lunii iulie.
 Ai dreptul de a solicita oricând modificarea sau ștergerea datelor tale de contact printr-o cerere transmisă la adresa de email contact@psiho-hub.info</small>
                       </p>
-                      <p className="mt-0 max-w-md mx-auto text-base text-gray-900 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-3">
+                      <p className="mt-0 max-w-md mx-auto text-base text-gray-900 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-1">
                         <small>IMPORTANT: Platforma de evaluare poate fi accesată de pe maxim 2 adrese de IP</small>
+                      </p>
+                      <p className="mt-0 max-w-md mx-auto text-base text-gray-900 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-3">
+                        <small>IMPORTANT: După testare demo ai acces la informații despre plată</small>
                       </p>
                   </div>
 
