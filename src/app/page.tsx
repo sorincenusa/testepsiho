fae34ee691c6ea3a240e6e2e5b1c23cc4e84c560
sorincenusa <sorincenusa@gmail.com>
2026-05-26 10:17:58 +0300

    Update contact agreement text for clarity

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -156,10 +156,10 @@ export default function Home() {
                           Pregătește-te eficient cu teste generate dinamic. Ai la dispoziție istoricul rezultatelor, rapoarte detaliate și algoritmi care pun accent pe capitolele pe care trebuie să le mai repeți.
                       </p>
                       <p className="mt-0 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-3">
-                        <small>Prin completarea formularului de contact, ești de acord cu prelucrarea datelor furnizate (nume și adresă de email) exclusiv în scopul accesăsii platformei de verificare cunoștințe.
-Datele tale sunt tratate confidențial și nu vor fi vândute, distribuite sau transmise către terți fără acordul tău, cu excepția situațiilor prevăzute de lege.
+                        <small>Prin înregistrare, ești de acord cu prelucrarea datelor furnizate (nume și adresă de email) exclusiv în scopul accesării platformei de verificare cunoștințe.
+Datele tale sunt confidențiale și nu vor fi vândute, distribuite sau transmise către terți fără acordul tău, cu excepția situațiilor prevăzute de lege.
 Luăm măsuri rezonabile pentru protejarea și securizarea informațiilor transmise prin intermediul acestui site. Datele vor fi șterse în cursul lunii iulie.
-Ai dreptul de a solicita oricând modificarea sau ștergerea datelor tale de contact printr-o cerere transmisă la adresa de email afișată pe site.</small>
+Ai dreptul de a solicita oricând modificarea sau ștergerea datelor tale de contact printr-o cerere transmisă la adresa de email contact@psiho-hub.info</small>
                       </p>
                   </div>
 
