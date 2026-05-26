7bd04d59a4bc0756cec78dc1137b11fee49b8a79
sorincenusa <sorincenusa@gmail.com>
2026-05-26 11:58:15 +0300

    Update IP access limit for evaluation platform

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -162,7 +162,7 @@ Luăm măsuri rezonabile pentru protejarea și securizarea informațiilor transm
 Ai dreptul de a solicita oricând modificarea sau ștergerea datelor tale de contact printr-o cerere transmisă la adresa de email contact@psiho-hub.info</small>
                       </p>
                       <p className="mt-0 max-w-md mx-auto text-base sm:text-lg md:mt-5 md:text-xl md:max-w-3xl mb-3">
-                        <small>IMPORTANT: Platforma de evaluare poate fi accesată de pe un singur IP</small>
+                        <small>IMPORTANT: Platforma de evaluare poate fi accesată de pe maxim 2 adrese de IP</small>
                       </p>
                   </div>
 
